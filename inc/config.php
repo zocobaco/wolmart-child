8c8c98394a8de8e9cac7ddeb8b2b25b498f287e9
zocoba <109752213+zocobaco@users.noreply.github.com>
2025-11-17 18:19:13 -0500

    Summary

--- inc/config.php
+++ inc/config.php
@@ -7,23 +7,6 @@ define('ZCB_PATH', get_theme_file_path());
 define('ZCB_ASSETS_URI', ZCB_URI . '/assets');
 define('ZCB_CSS_URI', ZCB_ASSETS_URI . '/css');
 
-// === GZIP ===
-add_action('init', function () {
-    if (!headers_sent() && extension_loaded('zlib') && !ini_get('zlib.output_compression')) {
-        ini_set('zlib.output_compression', 'On');
-        ini_set('zlib.output_compression_level', '6');
-    }
-});
-
-// === Canonical SEO ===
-add_action('wp_head', function () {
-    if (!is_singular()) return;
-    global $wp_query;
-    $obj = $wp_query->get_queried_object();
-    if (!empty($obj->ID) && is_numeric($obj->ID)) {
-        echo '<link rel="canonical" href="' . esc_url(get_permalink($obj->ID)) . '">' . "\n";
-    }
-});
 
 // === WebP ===
 add_filter('file_is_displayable_image', function ($result, $path) {
