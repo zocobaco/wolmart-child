a17f67c8af49fd7eb44dfd0d0acb72124bad91f8
zocoba <109752213+zocobaco@users.noreply.github.com>
2025-11-18 22:03:38 -0500

    up

--- functions.php
+++ functions.php
@@ -4,20 +4,3 @@
  * @package ZOCOBA WordPress Framework
  * @since 1.0
  */
-defined('ABSPATH') || exit;
-
-// Cargar archivos separados con nombres semánticos
-$includes = [
-    'inc/config.php',             // Configuración base y constantes
-    'inc/assets.php',             // Manejador de carga de CSS/JS
-    'inc/woo-hooks.php',          // ¡NUEVO! Personalizaciones de Hooks
-];
-
-foreach ($includes as $file) {
-    $filepath = get_theme_file_path($file);
-    if (file_exists($filepath)) {
-        require_once $filepath;
-    } else {
-        error_log("Archivo de inclusión no encontrado: $filepath");
-    }
-}
