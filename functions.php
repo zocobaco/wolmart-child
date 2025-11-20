4c64d285b0756697c93fe14bf0fa0c722e5e77ce
zocoba <109752213+zocobaco@users.noreply.github.com>
2025-11-20 11:31:56 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 defined( 'ABSPATH' ) || exit; // Seguridad: Evita acceso directo
 
 // 1. Definir Constantes Globales
-define( 'ZCB_VERSION', '2.1.0' );
+define( 'ZCB_VERSION', '1.0.0' );
 define( 'ZCB_PATH', get_stylesheet_directory() );
 define( 'ZCB_URI',  get_stylesheet_directory_uri() );
 
